081ddb350658f82b327dd2c64acc89995c5b66c1
sevillecarlos <sevillecarlos@gmail.com>
2021-07-16 13:53:31 -0600

    fix the search-query

--- src/helper/removeRepeatElements.ts
+++ src/helper/removeRepeatElements.ts
@@ -1,7 +1,6 @@
 export const removeRepeatElements = (arr: any) => {
   const paramsArr = arr;
   const uniqueArr = paramsArr.reduce((v: any, obj: { id: number }) => {
-    console.log(v.length)
     !v.some((o: { id: number }) => o.id === obj.id) && v.push(obj);
     return v;
   }, []);
